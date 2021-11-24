c80ece5daeef3fb6dd93636666297af65fa98b40
Jarek <jarekjanas95@gmail.com>
2021-11-24 21:51:24 +0100

    commit

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,4 @@
-//SERVER ROUTES
-// export const USER_SERVER = "/api/users";
-
-export const API_KEY = "64b10bf0325fe2cd1c56fe071b47d9f0";
+export const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const API_URL = "https://api.themoviedb.org/3/";
 
