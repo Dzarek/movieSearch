e7950ce725d2198b5cba35ba15467be6f32d57c7
Jarek <jarekjanas95@gmail.com>
2021-11-24 21:01:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+import { API_KEY, API_URL } from "./Config";
+import spinner from "./images/spinner.gif";
+
+const FEATURED_API = `${API_URL}discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&language=pl-PL&page=1`;
+const SEARCH_API = `${API_URL}search/movie?&api_key=${API_KEY}&query=`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const getData = async (API) => {
+    setLoading(true);
+    const moviesResponse = await fetch(API);
+    const moviesR = await moviesResponse.json();
+    setMovies([...movies, ...moviesR.results]);
+    setCurrentPage(moviesR.page);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData(FEATURED_API);
+  }, []);
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+    if (searchTerm) {
+      // ONE WAY
+      // const moviesResponse = await fetch(SEARCH_API + searchTerm);
+      // const moviesR = await moviesResponse.json();
+      // setMovies(moviesR.results);
+
+      // SECOND WAY
+      fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
+        });
+    }
+  };
+
+  // const handleOnChange = (e) => {
+  //   setSearchTerm(e.target.value);
+  // };
+
+  const handleClickBtn = () => {
+    let endpoint = `${API_URL}discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&language=pl-PL&page=${
+      currentPage + 1
+    }`;
+    getData(endpoint);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div>
+          <h2>Wyszukiwarka Filmów</h2>
+        </div>
+        <form onSubmit={handleOnSubmit} action="">
+          <input
+            type="text"
+            placeholder="szukaj..."
+            className="search"
+            value={searchTerm}
+            onChange={handleOnSubmit}
+          />
+        </form>
       </header>
-    </div>
+
+      <div className="movie-container">
+        {movies &&
+          movies.map((movie) => {
+            return <Movie key={movie.id} {...movie} />;
+          })}
+      </div>
+      {loading ? (
+        <div className="loader">
+          <img className="loader" src={spinner} alt="" />
+          <h2>Wczytywanie...</h2>
+        </div>
+      ) : (
+        <button className="seeMoreBtn" onClick={handleClickBtn}>
+          zobacz więcej
+        </button>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { IMAGE_URL } from "../Config";
+
+const IMG_API = `${IMAGE_URL}w1280`;
+
+const setVoteClass = (vote) => {
+  if (vote >= 8) {
+    return "lightgreen";
+  } else if (vote >= 6) {
+    return "orange";
+  } else {
+    return "darkred";
+  }
+};
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+  return (
+    <div className="movie">
+      <img
+        src={
+          poster_path
+            ? IMG_API + poster_path
+            : "https://cdn.pixabay.com/photo/2019/11/07/20/48/cinema-4609877_960_720.jpg"
+        }
+        alt={title}
+      />
+      <span
+        className="votesNumber"
+        style={{ color: `${setVoteClass(vote_average)}` }}
+      >
+        {vote_average}
+      </span>
+      <div className="movie-info">
+        <h3>{title}</h3>
+      </div>
+      <div className="movie-over">
+        <h2>Opis:</h2>
+        <p>{overview ? overview : "Brak szczegółów w bazie danych."}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
